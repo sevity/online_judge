cc0cd85108256f2b37f1c59bfe51f971d5008ec3
sevity <sevity@ymail.com>
2023-08-09 23:46:28 +0900

    CORS setting

---
+++ problem-service/src/main/kotlin/com/sevity/problemservice/config/WebConfig.kt
@@ -0,0 +1,21 @@
+package com.sevity.problemservice.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig {
+
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer {
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/**")  // 루트부터 모든 하위경로
+                    .allowedOrigins("http://sevity.com:9994", "http://localhost:9994")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE")
+            }
+        }
+    }
+}
