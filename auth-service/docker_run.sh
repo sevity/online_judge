f19d7d3b362e499afa60be49dd4b49a75ba43e82
sevity <sevity@ymail.com>
2023-08-10 02:03:43 +0900

    let it use host's redis

--- auth-service/docker_run.sh
+++ auth-service/docker_run.sh
@@ -2,7 +2,12 @@
 
 PORT=$1
 
-docker run -e SPRING_APPLICATION_JSON='{"server":{"port":'"$PORT"'}}' -p $PORT:$PORT -e DATABASE_URL=jdbc:postgresql://192.168.0.20:5432/online_judge -e DATABASE_USERNAME=online_judge_admin -e DATABASE_PASSWORD=abcd123$ auth-service
-
-
+docker run \
+  --network host \
+  -e SPRING_APPLICATION_JSON='{"server":{"port":'"$PORT"'}}' \
+  -p $PORT:$PORT \
+  -e DATABASE_URL=jdbc:postgresql://192.168.0.20:5432/online_judge \
+  -e DATABASE_USERNAME=online_judge_admin \
+  -e DATABASE_PASSWORD=abcd123$ \
+  auth-service
 
