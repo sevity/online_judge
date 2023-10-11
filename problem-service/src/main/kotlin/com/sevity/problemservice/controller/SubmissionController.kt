394b7f2d7c24e9511386515437dcdb53a6b19840
sevity <sevity@ymail.com>
2023-10-12 02:48:14 +0900

    grpc-client-spring-boot-autoconfigure 적용하여 포맷팅개선

--- problem-service/src/main/kotlin/com/sevity/problemservice/controller/SubmissionController.kt
+++ problem-service/src/main/kotlin/com/sevity/problemservice/controller/SubmissionController.kt
@@ -6,17 +6,18 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RestController
+import net.devh.boot.grpc.client.inject.GrpcClient
 
-import com.sevity.authservice.grpc.SesseionServiceGrpc;
-import com.sevity.authservice.grpc.SessionService.SessionRequest;
-import com.sevity.authservice.grpc.SessionService.UserResponse;
-
-
-import io.grpc.ManagedChannelBuilder
+import com.sevity.authservice.grpc.SesseionServiceGrpc
+import com.sevity.authservice.grpc.SessionService.SessionRequest
+import com.sevity.authservice.grpc.SessionService.UserResponse
 import java.lang.reflect.InvocationTargetException
 
 @RestController
 class SubmissionController {
+
+    @GrpcClient("authService")
+    private lateinit var sessionServiceStub: SesseionServiceGrpc.SesseionServiceBlockingStub
 
     @PostMapping("/submit")
     fun submitCode(request: HttpServletRequest): ResponseEntity<String> {
@@ -24,11 +25,9 @@ class SubmissionController {
         val sessionId = cookies?.find { it.name == "SESSION" }?.value
             ?: return ResponseEntity("Session ID not found", HttpStatus.UNAUTHORIZED)
 
-        val channel = ManagedChannelBuilder.forAddress("sevity.com", 50051).usePlaintext().build()
-        val stub = SesseionServiceGrpc.newBlockingStub(channel)
         val request2 = SessionRequest.newBuilder().setSessionId(sessionId).build()
         try {
-            val response = stub.getUserId(request2)
+            val response = sessionServiceStub.getUserId(request2)
             println("Received user ID: ${response.userId}")
             return ResponseEntity("Code submitted", HttpStatus.OK)
         } catch (e: InvocationTargetException) {
