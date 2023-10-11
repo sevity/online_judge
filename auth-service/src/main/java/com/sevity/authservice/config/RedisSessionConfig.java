b3a50244dbc871720f7bd7589de818bbd28af6dd
sevity <sevity@ymail.com>
2023-10-11 12:33:23 +0900

    세션id에서 redis를 통해 user id를 조회하도록 구현

--- auth-service/src/main/java/com/sevity/authservice/config/RedisSessionConfig.java
+++ auth-service/src/main/java/com/sevity/authservice/config/RedisSessionConfig.java
@@ -1,9 +1,18 @@
 package com.sevity.authservice.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+import io.lettuce.core.RedisClient;
+import io.lettuce.core.api.StatefulRedisConnection;
 
 @Configuration
 @EnableRedisHttpSession
 public class RedisSessionConfig {
+    @Bean
+    public StatefulRedisConnection<String, String> redisConnection() {
+        RedisClient redisClient = RedisClient.create("redis://localhost:6379");
+        return redisClient.connect();
+    }    
 }
--- auth-service/src/main/java/com/sevity/authservice/service/SessionServiceImpl.java
+++ auth-service/src/main/java/com/sevity/authservice/service/SessionServiceImpl.java
@@ -5,24 +5,46 @@ import com.sevity.authservice.domain.UserRepository;
 import com.sevity.authservice.grpc.SesseionServiceGrpc;
 import com.sevity.authservice.grpc.SessionService.SessionRequest;
 import com.sevity.authservice.grpc.SessionService.UserResponse;
+
+import io.grpc.StatusRuntimeException;
 import io.grpc.stub.StreamObserver;
+import io.grpc.Status;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import io.lettuce.core.api.StatefulRedisConnection;
+import io.lettuce.core.api.sync.RedisCommands;
 
 @Service
 public class SessionServiceImpl extends SesseionServiceGrpc.SesseionServiceImplBase {
 
     @Autowired
     private UserRepository userRepository;
+    private final StatefulRedisConnection<String, String> redisConnection;
+
+    @Autowired
+    public SessionServiceImpl(StatefulRedisConnection<String, String> redisConnection) {
+        this.redisConnection = redisConnection;
+    }
 
     @Override
     public void getUserId(SessionRequest request, StreamObserver<UserResponse> responseObserver) {
         String sessionId = request.getSessionId();
+
+        RedisCommands<String, String> redisCommands = redisConnection.sync();
+        String userId = redisCommands.get(sessionId); // Redis에서 세션 ID로 사용자 ID를 조회
+        if (userId == null) {
+            responseObserver.onError(new StatusRuntimeException(Status.NOT_FOUND));
+            return;
+        }
+
         // 세션 ID로 사용자 ID를 조회하는 로직
         //User user = userRepository.findBySessionId(sessionId); // 예시 코드, 실제 구현은 다를 수 있습니다.
         //String userId = user != null ? user.getId() : "Not Found";
-        int userId = 123;
-        UserResponse response = UserResponse.newBuilder().setUserId(userId).build();
+        int nUserId = Integer.parseInt(userId);
+
+        UserResponse response = UserResponse.newBuilder().setUserId(nUserId).build();
         responseObserver.onNext(response);
         responseObserver.onCompleted();
     }
